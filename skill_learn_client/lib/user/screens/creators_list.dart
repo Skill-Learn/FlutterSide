84afa7b143846f7884a15032898d3a1d79d05b51
Yafet Lulseged <se.yafet.lulseged@gmail.com>
2021-09-04 21:44:33 +0300

    Creators screen file and CreatorsList stateful widget created

---
+++ skill_learn_client/lib/user/screens/creators_list.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CreatorsList extends StatefulWidget {
+  @override
+  _CreatorsListState createState() => _CreatorsListState();
+}
+
+class _CreatorsListState extends State<CreatorsList> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
