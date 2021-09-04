501e6f43da924e58b9a929978ea709201c6f345c
Yafet Lulseged <se.yafet.lulseged@gmail.com>
2021-09-04 21:52:27 +0300

    Creators screen returns a listview widget

--- skill_learn_client/lib/user/screens/creators_list.dart
+++ skill_learn_client/lib/user/screens/creators_list.dart
@@ -13,6 +13,29 @@ class _CreatorsListState extends State<CreatorsList> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+            child: Card(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                      color: Color(0xFF42A5F5),
+                      width: 1,
+                      style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(50)),
+              child: ListTile(
+                onTap: () {},
+                title: Text(
+                  "Flutter is the easiest app platform",
+                  style: TextStyle(color: Color(0xFF42A5F5)),
+                ),
+                leading: FlutterLogo(),
+              ),
+            ),
+          );
+        });
   }
 }
