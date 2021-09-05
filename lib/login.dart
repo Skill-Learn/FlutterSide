4a792a9221a7a9252f07efc571f502b1d823a74c
Kirubel Teka <se.kirubel.teka@gmail.com>
2021-09-05 20:16:02 +0300

    email and password UI

--- lib/login.dart
+++ lib/login.dart
@@ -10,6 +10,7 @@ class _MyLoginPageState extends State<MyLoginPage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
             child: Stack(
@@ -17,16 +18,76 @@ class _MyLoginPageState extends State<MyLoginPage> {
                 Container(
                   padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
                   child: Text(
-                    'Hello World',
+                    'Hello',
                     style: TextStyle(
                       fontSize: 80.0,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                )
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 175.0, 0.0, 0.0),
+                  child: Text(
+                    'There',
+                    style: TextStyle(
+                      fontSize: 80.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
+                  child: Text(
+                    '.',
+                    style: TextStyle(
+                      fontSize: 80.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
               ],
             ),
-          )
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'EMAIL',
+                    labelStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'PASSWORD',
+                    labelStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,8 @@ class _SkillLearnState extends State<SkillLearn> {
       // mainAxisSize: MainAxisSize.max,
       children: [
         // VideoTile(),
-        HomePage(),
+
+        // HomePage(),
         SizedBox(
           height: 30,
         ),
@@ -41,29 +42,32 @@ class _SkillLearnState extends State<SkillLearn> {
         appBar: AppBar(
           title: Text("Skill Learn"),
         ),
-        body: tabs[_currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.explore),
-              label: "Creators",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "Profile",
-            ),
-          ],
-          onTap: (index) => {
-            setState(() {
-              _currentIndex = index;
-            })
-          },
-        ),
+
+        body: MyLoginPage(),
+
+        //  body: tabs[_currentIndex],
+        // bottomNavigationBar: BottomNavigationBar(
+        //   currentIndex: _currentIndex,
+        //   items: [
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.home),
+        //       label: "Home",
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.explore),
+        //       label: "Creators",
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.person),
+        //       label: "Profile",
+        //     ),
+        //   ],
+        //   onTap: (index) => {
+        //     setState(() {
+        //       _currentIndex = index;
+        //     })
+        //   },
+        // ),
       ),
     );
   }
